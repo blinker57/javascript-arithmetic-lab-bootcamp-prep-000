b70815b5ec80ea20457f9cc6e627ea28cb6773a7
Bruce Linker <bruce@linkerhouse.com>
2018-01-14 21:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ function add(a, b) {
   return a + b
 }
 
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
 function add5() {
   number += 5
 }
@@ -12,8 +24,22 @@ function divideBy3() {
   number /= 3
 }
 
-console.log(number)
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
 
 function makeInt(n) {
-
+  return parseInt(n, 10)
 }
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+console.log(number)
+
+
